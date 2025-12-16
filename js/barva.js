3c63b74a8428cfbcc75ce8bad4e8623615531d76
TilenCecko <167509679+TilenCecko@users.noreply.github.com>
2025-12-16 20:26:01 +0100

    Add files via upload

---
+++ js/barva.js
@@ -0,0 +1,8 @@
+// Pridobi elemente
+const colorPicker = document.getElementById("colorPicker");
+const logo = document.getElementById("logo");
+
+// Dogodek ob spremembi barve
+colorPicker.addEventListener("input", () => {
+    logo.setAttribute("fill", colorPicker.value);
+});
