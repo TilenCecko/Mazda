3f5d8a0549be60ac931c06967bee07b8da2bc48c
TilenCecko <167509679+TilenCecko@users.noreply.github.com>
2026-01-16 17:08:25 +0100

    Add files via upload

---
+++ js/sweetalerts.js
@@ -0,0 +1,14 @@
+const mojaTocka = document.getElementById("mojaTocka2");
+
+mojaTocka.addEventListener("click", () => {
+    Swal.fire({
+        title: 'Avtor',
+        text: 'Tilen Čečko',
+        icon: 'info',
+        confirmButtonText: 'V redu'
+    });
+});
+
+
+
+
